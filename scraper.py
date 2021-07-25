894cf9b7ad7bc90ad71f7139d64fa8e4b6ccb547
fouadsan <f.benayad95@gmail.com>
2021-07-25 13:29:12 +0100

    Scraped my portfolio services section

--- scraper.py
+++ scraper.py
@@ -1,19 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-with open('src/test_example.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+# Scraping my personal website 😂 (Services section) and export infos to csv file.
 
-# result = soup.find('h1', id='big_title')
-articles = soup.find_all('div', class_='article')
+source = requests.get('https://fouadben.herokuapp.com').text
 
-# print(article)
+soup = BeautifulSoup(source, 'lxml')
 
-for article in articles:
-    title = article.h2.a.text
+csv_file = open('scraped.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+
+csv_writer.writerow(['title', 'description', 'icon_link'])
+
+services = soup.find_all('div', class_='service-item')
+for service in services:
+    title = service.h4.text
     print(title)
-
-    description = article.p.text
+    description = service.p.text
     print(description)
 
+    try:  # to avoid code break in case there is no icon in the service item, you can apply it above also.
+        icon_src = service.img['src']
+        print(icon_src)
+    except Exception as e:
+        icon_src = None
+
     print()
+
+    csv_writer.writerow([title, description, icon_src])
+
+csv_file.close()
