536ff272fa438d64a850388a48293d623db3d8b0
fouadsan <f.benayad95@gmail.com>
2021-07-25 12:35:07 +0100

    Apllied on local html file

---
+++ scraper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('src/test_example.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+# result = soup.find('h1', id='big_title')
+articles = soup.find_all('div', class_='article')
+
+# print(article)
+
+for article in articles:
+    title = article.h2.a.text
+    print(title)
+
+    description = article.p.text
+    print(description)
+
+    print()
